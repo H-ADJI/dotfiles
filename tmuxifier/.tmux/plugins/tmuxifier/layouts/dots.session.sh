5392c8414ee9be478f29f20595513d13b453fa8e
khalil hadji <h-adji_tech@proton.me>
2025-04-11 12:41:37 +0200

    fix tmux

--- tmuxifier/.tmux/plugins/tmuxifier/layouts/dots.session.sh
+++ tmuxifier/.tmux/plugins/tmuxifier/layouts/dots.session.sh
@@ -1,29 +1,18 @@
 # Set a custom session root path. Default is `$HOME`.
 # Must be called before `initialize_session`.
-session_root "~/.dotfiles/"
-
 # Create session with specified name if it does not already exist. If no
 # argument is given, session name will be based on layout file name.
 if initialize_session "dotfiles"; then
 
     # Create a new window inline within session layout definition.
-    new_window "shell"
-
-    window_root "~/.dotfiles/nvim/.config/nvim/"
-    new_window "nvim config"
-    run_cmd "nv"
-
-    window_root "~/.dotfiles/zsh/"
-    new_window "zsh config"
-    run_cmd "nv"
-
-    window_root "~/.dotfiles/ansible/"
-    new_window "ansible playbooks"
-    run_cmd "nv"
+    window_root "~/dotfiles"
+    new_window "dotfiles"
 
     window_root "~/cyborg/"
     new_window "cyborg"
-    run_cmd "nv"
+
+    window_root "~/.dotfiles"
+    new_window "old dotfiles"
 
     select_window 1
 fi
