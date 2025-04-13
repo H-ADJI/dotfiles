95d47f19b0a6805509754c06d612e2f8ea6746fa
khalil hadji <h-adji_tech@proton.me>
2025-04-13 09:12:05 +0200

    add android devices aliases

--- zsh/zsh/lib/android.sh
+++ zsh/zsh/lib/android.sh
@@ -1,5 +1,9 @@
 #!/bin/zsh
 
-alias list_avd_devices="ls /home/khalil/.android/avd"
-alias launch_avd_device='/home/khalil/Android/Sdk/emulator/emulator -avd Pixel_XL_API_31 -writable-system '
-alias androidStudio="/home/khalil/android-studio/bin/studio"
+# android studio
+# alias list_avd_devices="ls /home/khalil/.android/avd"
+# alias launch_avd_device='/home/khalil/Android/Sdk/emulator/emulator -avd Pixel_XL_API_31 -writable-system '
+# alias androidStudio="/home/khalil/android-studio/bin/studio"
+alias adb_root="adb shell setprop persist.sys.root_access 3 && adb root"
+alias list_avd_devices="gmtool admin list"
+alias launch_avd_device="/opt/genymotion/player  --vm-name 'Samsung Galaxy S23'"
