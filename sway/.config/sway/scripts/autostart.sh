79d0dc30b56b2f5ee9724ed058bb64f2ce73f2b1
khalil hadji <h-adji_tech@proton.me>
2025-05-16 12:25:45 +0200

    change autostart startegy

--- sway/.config/sway/scripts/autostart.sh
+++ sway/.config/sway/scripts/autostart.sh
@@ -1,27 +1,20 @@
 #!/bin/bash
 
 # Launch applications in the background
-alacritty &
-brave &
-slack &
-spotify &
 
 # Wait briefly to ensure the applications have time to start
+
+alacritty &
 sleep 1
 
-# Move Alacritty to workspace 1
-swaymsg '[app_id="Alacritty"] move to workspace 1'
-# swaymsg 'workspace 1'
+swaymsg 'workspace 2'
+brave &
+sleep 1
 
-# Move Brave to workspace 2
-swaymsg '[app_id="brave-browser"] move to workspace 2'
+swaymsg 'workspace 3'
+slack &
+sleep 1
 
-# Move Spotify to workspace 2
-swaymsg '[class="Spotify"] move to workspace 2'
-# swaymsg 'workspace 2'
-
-# Move Slack to workspace 3
-swaymsg '[app_id="slack"] move to workspace 3'
-# swaymsg 'workspace 3'
+swaymsg 'workspace 1'
 
 exit 0
