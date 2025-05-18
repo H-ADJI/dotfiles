09c552820ca9b571b671c7e235d2ad919d3a58a6
khalil hadji <h-adji_tech@proton.me>
2025-05-18 09:36:14 +0200

    timer change

--- sway/.config/sway/scripts/focus_timer.sh
+++ sway/.config/sway/scripts/focus_timer.sh
@@ -1,23 +1,15 @@
 #!/bin/sh
-# Set default duration in seconds (e.g., 20 minutes = 1200 seconds)
-DURATION=2700
+DURATION=3600
 notification_timeout=8000
 APP_NAME="Timer"
 STEPS=30
 LOG_FILE="$HOME/.timer.log"
 
-hours=$((DURATION / 3600))
-minutes=$(((DURATION % 3600) / 60)) # Get remaining minutes
-
-# Format as HH:MM
-hours=$(printf "%02d" $hours)
-minutes=$(printf "%02d" $minutes)
-
 notify() {
     notify-send "$1" "$2" -t "$notification_timeout" -a "$APP_NAME"
     paplay /usr/share/sounds/freedesktop/stereo/complete.oga
 }
-notify "Timer Activated" "Your ${hours}h$minutes of focused work has started !"
+notify "Timer Activated" "1 Hour of focus started !"
 date +%s >"$LOG_FILE"
 
 while [ $DURATION -ge 0 ]; do
