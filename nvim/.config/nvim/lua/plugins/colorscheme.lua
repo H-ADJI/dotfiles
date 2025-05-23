5956ad3c829b25c110b5de99ab2c2abe58ef0d94
khalil hadji <h-adji_tech@proton.me>
2025-05-23 09:46:45 +0200

    toggle colorscheme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -5,6 +5,22 @@ return {
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
+    keys = {
+      {
+        "<leader>tw",
+        function()
+          vim.cmd("colorscheme default")
+        end,
+        desc = "toggle white colorscheme",
+      },
+      {
+        "<leader>tn",
+        function()
+          vim.cmd("colorscheme catppuccin-mocha")
+        end,
+        desc = "toggle dark colorscheme",
+      },
+    },
     opts = {
       colorscheme = "catppuccin-mocha",
     },
