17de79b78745b35cdf9423128ad89748f4340c7b
khalil hadji <h-adji_tech@proton.me>
2025-06-28 09:03:29 +0200

    vim keybinds

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -6,17 +6,25 @@ vim.keymap.set({ "n", "v", "i", "x" }, "<right>", '<cmd>echo "Use vim motion !"<
 vim.keymap.set({ "n", "v", "i", "x" }, "<up>", '<cmd>echo "Use vim motion !"<CR>')
 vim.keymap.set({ "n", "v", "i", "x" }, "<down>", '<cmd>echo "Use vim motion !"<CR>')
 vim.keymap.set({ "v", "x" }, "<leader>cn", ":CarbonNow<CR>", { desc = "Capture Code snippet", silent = true })
-vim.api.nvim_set_keymap(
-  "n",
-  "<leader>Fp",
-  ':let @+ = expand("%:p")<CR>:echo expand("%:p")<CR>',
-  { noremap = true, silent = true, desc = "Copy Buffer Full Path" }
-)
+vim.keymap.set("n", "zz", function()
+  vim.cmd("q")
+end, { desc = "Quit shortcut" })
 
-vim.keymap.set("n", "<leader>sp", function()
-  local file_path = vim.fn.expand("%:p") -- Full path
+vim.keymap.set("n", "<leader>pp", function()
+  local file_path = vim.fn.expand("%:p")
   if file_path == "" then
     file_path = "[No file opened]"
   end
   print(file_path)
-end, { desc = "Show current file path" })
+  vim.fn.setreg("+", file_path)
+end, { desc = "Show and copy current file path" })
+
+vim.keymap.set("n", "<leader>fx", function()
+  local file_path = vim.fn.expand("%:p") -- Full path of current buffer
+  if file_path == "" then
+    vim.notify("[No file opened]", vim.log.levels.ERROR)
+    return
+  end
+  vim.fn.system("chmod +x " .. vim.fn.shellescape(file_path))
+  vim.notify("Made " .. file_path .. " executable", vim.log.levels.INFO)
+end, { desc = "Make [F]ile e[X]ecutable (chmod +x)" })
