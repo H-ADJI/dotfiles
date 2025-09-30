b42d3fb682164d5c2a50dcc6b5c52f0e34702eb3
khalil hadji <h-adji_tech@proton.me>
2025-09-30 13:00:32 +0200

    focus timer

--- sway/.config/sway/scripts/focus_timer.sh
+++ sway/.config/sway/scripts/focus_timer.sh
@@ -1,21 +1,17 @@
 #!/bin/sh
-DURATION=5400
+DURATION=$(zenity --entry --title="Timer" --text="Enter duration (minutes)")
 notification_timeout=8000
 APP_NAME="Timer"
-STEPS=30
 LOG_FILE="$HOME/.timer.log"
 
 notify() {
     notify-send "$1" "$2" -t "$notification_timeout" -a "$APP_NAME"
     paplay /usr/share/sounds/freedesktop/stereo/complete.oga
 }
-notify "Timer Activated" "90 minutes of focused work started !"
+notify "Timer Activated" "$DURATION minutes of focused work started !"
 date +%s >"$LOG_FILE"
 
-while [ $DURATION -ge 0 ]; do
-    sleep $STEPS
-    DURATION=$((DURATION - STEPS))
-done
+sleep "$((DURATION * 60))"
 
 rm -f "$LOG_FILE"
 notify "Time is up" "Time to rest your eyes and take a walk!"
