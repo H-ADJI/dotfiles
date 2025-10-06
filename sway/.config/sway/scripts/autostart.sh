c7f7f6a294484d3f5f6fb761dc34e86744d64b17
khalil hadji <h-adji_tech@proton.me>
2025-10-06 10:24:03 +0200

    update autostart.sh

--- sway/.config/sway/scripts/autostart.sh
+++ sway/.config/sway/scripts/autostart.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 # Launch applications in the background
+echo 'connect 10:94:97:36:C7:15' | bluetoothctl
 alacritty &
 sleep 2
 swaymsg 'workspace 2'
-brave &
-echo 'connect 10:94:97:36:C7:15' | bluetoothctl
+firefox &
+sleep 2
+swaymsg 'workspace 3'
+slack &
+spotify &
+sleep 3
+swaymsg 'workspace 2'
 
 exit 0
