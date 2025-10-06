65d7fe7b6c946e0e626a723e8f486c9560812fa8
khalil hadji <h-adji_tech@proton.me>
2025-10-06 14:21:04 +0200

    connect blth

--- sway/.config/sway/scripts/autostart.sh
+++ sway/.config/sway/scripts/autostart.sh
@@ -7,8 +7,8 @@ sleep 2
 swaymsg 'workspace 3'
 slack &
 spotify &
+echo 'connect 10:94:97:36:C7:15' | bluetoothctl
 sleep 3
 swaymsg 'workspace 2'
-echo 'connect 10:94:97:36:C7:15' | bluetoothctl
 
 exit 0
