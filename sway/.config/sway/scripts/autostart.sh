4d7b1006a802108d1eca53d951dc0ed5d2ecada7
khalil hadji <h-adji_tech@proton.me>
2025-10-14 22:38:39 +0200

    autostart bluetooth

--- sway/.config/sway/scripts/autostart.sh
+++ sway/.config/sway/scripts/autostart.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-alacritty &
-sleep 2
-swaymsg 'workspace 2'
-brave --ozone-platform=wayland --ozone-platform-hint=wayland &
-sleep 2
-swaymsg 'workspace 3'
-slack &
+sleep 5
 echo 'connect 10:94:97:36:C7:15' | bluetoothctl
-sleep 2
-swaymsg 'workspace 2'
-
 exit 0
