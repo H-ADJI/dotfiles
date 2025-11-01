fb4d0538c537f81190bb7c52d79bf69af830f592
khalil hadji <h-adji_tech@proton.me>
2025-11-01 06:56:05 +0100

    add tmuxifier aliases

--- zsh/zsh/lib/tmuxifier.sh
+++ zsh/zsh/lib/tmuxifier.sh
@@ -1,7 +1,7 @@
 PATH=$PATH:~/.tmux/plugins/tmuxifier/bin
 _evalcache tmuxifier init -
-alias work="tmuxifier s scraping"
-alias dicli="tmuxifier s dicli"
+alias scraping="tmuxifier s scraping"
+alias cli="tmuxifier s scraping-cli"
 alias nvraw="tmuxifier s nvimraw"
 alias dsa="tmuxifier s dsa"
 alias dots="tmuxifier s dots"
