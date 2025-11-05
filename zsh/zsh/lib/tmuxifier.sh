35a1818888478eb951bd5d05b3f9d7a8c6ffaf99
khalil hadji <h-adji_tech@proton.me>
2025-11-05 10:13:25 +0100

    fix aliases and add direnv whitelist

--- zsh/zsh/lib/tmuxifier.sh
+++ zsh/zsh/lib/tmuxifier.sh
@@ -1,7 +1,7 @@
 PATH=$PATH:~/.tmux/plugins/tmuxifier/bin
 _evalcache tmuxifier init -
 alias scraping="tmuxifier s scraping"
-alias cli="tmuxifier s scraping-cli"
+alias cli="tmuxifier s cli"
 alias nvraw="tmuxifier s nvimraw"
 alias dsa="tmuxifier s dsa"
 alias dots="tmuxifier s dots"
