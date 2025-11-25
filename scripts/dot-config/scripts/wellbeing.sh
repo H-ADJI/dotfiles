344bab995707a3f89eff3a038896c4d073079b06
khalil hadji <h-adji_tech@proton.me>
2025-11-25 17:41:08 +0100

    add wellbeing and fix idle timers

---
+++ scripts/dot-config/scripts/wellbeing.sh
@@ -0,0 +1,12 @@
+# TODO: timer with snore gum and alacritty and notify-send
+notification_timeout=6000
+APP_NAME="Wellbeing Timer"
+notify() {
+    notify-send "$1" "$2" -t "$notification_timeout" -a "$APP_NAME"
+    paplay /usr/share/sounds/freedesktop/stereo/complete.oga
+}
+input=$(gum input --placeholder "Session duration in minutes [default is 90]")
+duration=${input:- 90}
+gum log -l info "Starting a $duration minutes session"
+snore "$duration"m
+notify "Time is up" "Time to rest your eyes and take a walk!"
