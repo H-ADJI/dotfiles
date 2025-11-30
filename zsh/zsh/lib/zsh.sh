6ac5e136a7b19ba1ad2eb45bbdde91a720273cf7
khalil hadji <h-adji_tech@proton.me>
2025-11-30 07:54:04 +0100

    fix keybinds

--- zsh/zsh/lib/zsh.sh
+++ zsh/zsh/lib/zsh.sh
@@ -5,5 +5,5 @@ alias xel="fc"
 alias fix_history="rm ~/.zsh_history && cp ~/dotfiles/zsh/.zsh_history ~"
 
 # Remap Ctrl-A and Ctrl-E
-bindkey "^n^l" end-of-line
-bindkey '^n^h' beginning-of-line
+bindkey "^x^l" end-of-line
+bindkey '^x^h' beginning-of-line
