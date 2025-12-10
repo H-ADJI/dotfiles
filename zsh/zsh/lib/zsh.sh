df04752fffcc56566863cbc9bf6949a106f73630
khalil hadji <h-adji_tech@proton.me>
2025-12-10 20:18:58 +0100

    sync history

--- zsh/zsh/lib/zsh.sh
+++ zsh/zsh/lib/zsh.sh
@@ -3,6 +3,7 @@ alias zshconfig="cd ~/zsh && nv"
 alias zbench="time zsh -i -c exit"
 alias xel="fc"
 alias fix_history="rm ~/.zsh_history && cp ~/dotfiles/zsh/.zsh_history ~"
+alias sync_history="cp ~/.zsh_history ~/dotfiles/zsh/"
 
 # Remap Ctrl-A and Ctrl-E
 # bindkey "^x^l" end-of-line
