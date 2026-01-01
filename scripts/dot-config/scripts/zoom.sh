3703d6ee60b8d87d08b41edd6fcdc15ccde79c65
khalil hadji <h-adji_tech@proton.me>
2026-01-01 18:11:21 +0100

    reduce zoom

--- scripts/dot-config/scripts/zoom.sh
+++ scripts/dot-config/scripts/zoom.sh
@@ -5,5 +5,5 @@ current=$(hyprctl -j getoption cursor:zoom_factor | jq -r '.float')
 if awk "BEGIN{exit !($current > 1.0)}"; then
     hyprctl keyword cursor:zoom_factor 1.0
 else
-    hyprctl keyword cursor:zoom_factor 2.5
+    hyprctl keyword cursor:zoom_factor 2
 fi
