01541ff44d453588b9b9557cca39cebeec0ea0de
khalil hadji <h-adji_tech@proton.me>
2026-01-01 10:33:04 +0100

    simplify setup

--- init.sh
+++ init.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-DISTRO=$(awk '/^ID=/' /etc/*-release | awk -F'=' '{ print tolower($2) }')
+sudo pacman -Syu --noconfirm
+toInstall=(
+    "base-devel"
+    "git"
+    "vim"
+    "go"
+    "gum"
+)
+sudo pacman -S --noconfirm --noprogressbar --needed --disable-download-timeout "${toInstall[@]}"
+[ ! -d "dotfiles" ] && git clone https://github.com/H-ADJI/dotfiles
+cd dotfiles || exit 1
 sudo --validate
-bash "$HOME/dotfiles/setup/$DISTRO/setup.sh"
+bash "$HOME/dotfiles/setup/setup.sh"
