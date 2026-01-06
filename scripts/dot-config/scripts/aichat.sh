b301dc508e6a522bfdab6f32d52902af4258ffe7
khalil hadji <h-adji_tech@proton.me>
2026-01-06 20:16:49 +0100

    revert tmp file write

--- scripts/dot-config/scripts/aichat.sh
+++ scripts/dot-config/scripts/aichat.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 MSG_DIR="$HOME/.aichat/"
+tmp_file=$(mktemp --suffix=.md)
 [ ! -d ~/.aichat ] && mkdir "$MSG_DIR"
 file="$HOME/.aichat/$(basename "$(pwd)").md"
 touch "$file"
@@ -8,4 +9,6 @@ echo "---" >>"$file"
 echo >>"$file"
 wl-paste >>"$file"
 
-tmux new-window -n 'aichat output' "$EDITOR $file"
+wl-paste >>"$tmp_file"
+
+tmux new-window -n 'aichat output' "$EDITOR $tmp_file"
