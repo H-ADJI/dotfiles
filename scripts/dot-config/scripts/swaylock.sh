8652f90e38684a59a4652a8d6b3092da1c8dd565
khalil hadji <h-adji_tech@proton.me>
2026-01-13 22:41:50 +0100

    change wallpapers

--- scripts/dot-config/scripts/swaylock.sh
+++ scripts/dot-config/scripts/swaylock.sh
@@ -3,15 +3,13 @@ sleep 0.3
 swaylock \
     --screenshots \
     --hide-keyboard-layout \
-    --indicator-radius 100 \
-    --indicator-thickness 7 \
-    --ring-color cba6f7 \
+    --indicator-radius 180 \
+    --indicator-thickness 20 \
+    --ring-color ffffff   \
     --ring-ver-color 89b4fa \
     --ring-wrong-color f38ba8 \
     --ring-clear-color a6e3a1 \
     --key-hl-color 1e1e2e \
-    --indicator-radius 100 \
-    --indicator-thickness 7 \
     --effect-blur 7x5 \
     --fade-in 0.2 \
     --effect-vignette 0.5:0.5 \
@@ -23,7 +21,7 @@ swaylock \
     --line-wrong-color 00000000 \
     --line-clear-color 00000000 \
     --separator-color 00000000 \
-    --inside-color cba6f7 \
+    --inside-color 00000000  \
     --inside-ver-color 89b4fa --inside-wrong-color f38ba8 \
     --inside-clear-color a6e3a1 \
     --color 1e1e2e80 \
