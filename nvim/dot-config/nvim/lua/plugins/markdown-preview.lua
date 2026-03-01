a4e5611b724ef99f3a62e7ce1fb8ee398a134c0d
khalil hadji <h-adji_tech@proton.me>
2026-03-01 12:46:28 +0100

    add markdown preview plugin

---
+++ nvim/dot-config/nvim/lua/plugins/markdown-preview.lua
@@ -0,0 +1,25 @@
+return {
+  "toppair/peek.nvim",
+  event = { "VeryLazy" },
+  build = "deno task --quiet build:fast",
+  keys = {
+    {
+      "<leader>tm",
+      function()
+        local peek = require("peek")
+        if peek.is_open() then
+          vim.notify("Markdown Preview Off")
+          require("peek").close()
+        else
+          vim.notify("Markdown Preview On")
+          require("peek").open()
+        end
+      end,
+      desc = "Typst Preview Toggle",
+    },
+  },
+  opts = {
+    app = { "google-chrome-stable", "--new-window" },
+    theme = "light", -- 'dark' or 'light'
+  },
+}
