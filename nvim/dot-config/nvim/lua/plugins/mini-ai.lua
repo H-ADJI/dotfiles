767c8d2900a07d00ba207fb3de109f8d6dee8a82
khalil hadji <h-adji_tech@proton.me>
2026-04-22 20:38:35 +0200

    remove mini modules

--- nvim/dot-config/nvim/lua/plugins/mini-ai.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "nvim-mini/mini.ai",
-  version = false,
-  enabled = false,
-  opts = {},
-}
--- nvim/dot-config/nvim/lua/plugins/mini-icons.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "nvim-mini/mini.icons",
-  version = false,
-  enabled = false,
-  opts = {},
-}
