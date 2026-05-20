7bd6d3a75760018a1968c6b62d34c2613bc32125
khalil hadji <h-adji_tech@proton.me>
2026-05-20 14:52:09 +0200

    revert todocomments config

--- nvim/dot-config/nvim/lua/plugins/todo-comments.lua
+++ nvim/dot-config/nvim/lua/plugins/todo-comments.lua
@@ -1,1 +1,21 @@
-return {}
+return {
+  "folke/todo-comments.nvim",
+  event = "VeryLazy",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  keys = {
+    {
+      "<leader>st",
+      function()
+        Snacks.picker.todo_comments()
+      end,
+      desc = "Search Todo Comments",
+    },
+    {
+      "<leader>sT",
+      function()
+        Snacks.picker.todo_comments({ keywords = { "TODO", "FIX", "FIXME" } })
+      end,
+      desc = "Search Todo/Fix/Fixme",
+    },
+  },
+}
