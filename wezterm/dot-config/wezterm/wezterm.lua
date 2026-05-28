f0297be6b8e9dc7088b91f5cce37010bd8171ef2
khalil hadji <h-adji_tech@proton.me>
2026-05-28 13:34:08 +0200

    add wezterm

---
+++ wezterm/dot-config/wezterm/wezterm.lua
@@ -0,0 +1,18 @@
+-- TODO: launch menu for tuis / clis
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+config.color_scheme = "Catppuccin Latte"
+
+config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Bold", italic = false })
+config.font_size = 15
+
+config.enable_tab_bar = false
+config.window_padding = {
+	left = "16cell",
+	right = "16cell",
+	top = "3cell",
+	bottom = "4cell",
+}
+
+return config
