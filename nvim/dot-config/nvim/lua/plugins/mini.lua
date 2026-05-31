9b35f11e4a5964cf9c4e0e21b6efceab90ff0c60
khalil hadji <h-adji_tech@proton.me>
2026-05-31 10:27:34 +0200

    remove mini pick

--- nvim/dot-config/nvim/lua/plugins/mini.lua
+++ nvim/dot-config/nvim/lua/plugins/mini.lua
@@ -9,16 +9,6 @@ return {
     local hipatterns = require("mini.hipatterns")
     hipatterns.setup({ highlighters = { hex_color = hipatterns.gen_highlighter.hex_color() } })
 
-    local win_config = function()
-      local height = math.floor(0.6 * vim.o.lines)
-      local width = math.floor(0.6 * vim.o.columns)
-      local start_row = math.floor(0.5 * (vim.o.lines - height))
-      local start_col = math.floor(0.5 * (vim.o.columns - width))
-      return { anchor = "NW", height = height, width = width, row = start_row, col = start_col }
-    end
-    require("mini.pick").setup({ window = { config = win_config } })
-    vim.api.nvim_set_hl(0, "MiniPickMatchCurrent", { bg = "#aec2e7" })
-
     require("mini.surround").setup({
       mappings = { add = "gsa", delete = "gsd", find = "gsf", find_left = "gsF", highlight = "gsh", replace = "gsr" },
       n_lines = 20,
@@ -27,20 +17,13 @@ return {
 
   keys = {
     {
-      "<leader>fg",
-      function()
-        MiniPick.builtin.grep()
-      end,
-      desc = "[F]ile [G]rep",
-    },
-    {
       "<leader>fe",
       function()
         if not MiniFiles.close() then
           MiniFiles.open()
         end
       end,
-      desc = "Open Mini Files",
+      desc = "Open MiniFiles",
     },
     {
       "<Esc>",
@@ -49,7 +32,7 @@ return {
           vim.cmd(":nohlsearch")
         end
       end,
-      desc = "Open Mini Files",
+      desc = "Close MiniFiles",
     },
   },
 }
