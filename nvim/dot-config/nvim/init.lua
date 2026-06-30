1f2790b7c6fa29fb53c9d8a7ab147ed14b7ee67e
khalil hadji <h-adji_tech@proton.me>
2026-06-30 23:52:50 +0200

    fix todos

--- nvim/dot-config/nvim/init.lua
+++ nvim/dot-config/nvim/init.lua
@@ -1,6 +1,5 @@
 -- options before plugins
 -- TODO: json schema integration
--- TODO: embedded scripts lsp / Treesitter integration
 require("config.options")
 require("config.auto-cmd")
 require("config.keybinds")
