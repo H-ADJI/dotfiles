8a9e3dd93f1e895683c9f4b2a46364e13587434e
khalil hadji <h-adji_tech@proton.me>
2026-07-06 15:25:34 +0200

    add hunk config / remove transparent background

--- nvim/dot-config/nvim/lua/plugins/colorscheme.lua
+++ nvim/dot-config/nvim/lua/plugins/colorscheme.lua
@@ -6,7 +6,7 @@ return {
     priority = 1000,
     config = function()
       local catppuccin = require("catppuccin")
-      catppuccin.setup({ transparent_background = true })
+      catppuccin.setup({ transparent_background = false })
       catppuccin.load("latte")
 
       vim.api.nvim_set_hl(0, "CursorLine", { bg = "#cddbf0" })
@@ -20,7 +20,7 @@ return {
     enabled = false,
     priority = 1000,
     config = function()
-      require("github-theme").setup({ options = { transparent = true } })
+      require("github-theme").setup({ options = { transparent = false } })
       vim.cmd("colorscheme github_light_high_contrast")
       vim.api.nvim_set_hl(0, "CursorLine", { bg = "#cddbf0" })
       vim.api.nvim_set_hl(0, "Visual", { bg = "#aec2e7" })
