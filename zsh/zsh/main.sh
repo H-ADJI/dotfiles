f88a9b7be9a0e3f4e8745977b1e15c3eadd9a27c
khalil hadji <h-adji_tech@proton.me>
2026-07-14 22:07:24 +0200

    add : zellij setup

--- zsh/zsh/main.sh
+++ zsh/zsh/main.sh
@@ -6,12 +6,13 @@ source ~/zsh/lib/aichat.sh
 source ~/zsh/lib/aliases.sh
 source ~/zsh/lib/bat.sh
 source ~/zsh/lib/completions.sh
+source ~/zsh/lib/fzf.sh
 source ~/zsh/lib/history.sh
 source ~/zsh/lib/keybinds.sh
-source ~/zsh/lib/fzf.sh
-source ~/zsh/lib/tv.sh
 source ~/zsh/lib/mise.sh
 source ~/zsh/lib/nvim.sh
 source ~/zsh/lib/path.sh
+source ~/zsh/lib/tv.sh
+source ~/zsh/lib/zellij.sh
 
 zinit cdreplay -q
