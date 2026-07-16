e6268167d065e98875410b737212efbc10951664
khalil hadji <h-adji_tech@proton.me>
2026-07-16 20:44:09 +0200

    fix: remove prompts and fix order

--- arch/setup/main.sh
+++ arch/setup/main.sh
@@ -16,24 +16,16 @@ sudo pacman -S --noconfirm --noprogressbar --needed --disable-download-timeout "
 SETUP_DIR="dotfiles/arch/setup/lib"
 
 # TODO: early return for manual testing
-# TODO: activate prompts using args
-gum confirm "install AUR helper"
 bash "$SETUP_DIR/aur_helper.sh"
 
-gum confirm "install packages"
 bash "$SETUP_DIR/packages.sh"
 
-gum confirm "Setup projects"
-bash "$SETUP_DIR/projects.sh"
-
-gum confirm "Decrypt secrets"
-bash "$SETUP_DIR/secrets.sh"
-
-gum confirm "Link dotfiles"
 bash "$SETUP_DIR/dotfiles.sh"
 
-gum confirm "install extra packages"
+bash "$SETUP_DIR/secrets.sh"
+
+bash "$SETUP_DIR/projects.sh"
+
 bash "$SETUP_DIR/extra_packages.sh"
 
-gum confirm "Set system state"
 bash "$SETUP_DIR/system_state.sh"
