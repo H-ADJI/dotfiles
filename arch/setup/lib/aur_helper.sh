5d7cbc611eaf9df539e477b49a08e983e996d89d
khalil hadji <h-adji_tech@proton.me>
2026-07-16 21:13:32 +0200

    update: add packages to build steps for cache

--- arch/setup/lib/aur_helper.sh
+++ arch/setup/lib/aur_helper.sh
@@ -1,4 +1,8 @@
+gum log -l info "[START] AUR helper"
+
 [ ! -d "yay" ] && git clone --depth 1 https://aur.archlinux.org/yay.git
 cd "yay" || exit 1
 makepkg -si --noconfirm
 cd || exit 1
+
+gum log -l info "[DONE] AUR helper"
