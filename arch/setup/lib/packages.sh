f917a0e0aec3ad8ac62a74fadc3a4203de2ecdb1
khalil hadji <h-adji_tech@proton.me>
2026-07-16 21:57:26 +0200

    fix: separate aur / official packages

--- arch/setup/lib/packages.sh
+++ arch/setup/lib/packages.sh
@@ -1,6 +1,8 @@
 SETUP_DIR="dotfiles/arch/setup/lib"
 
 gum log -l info "[START] Installing packages"
+sudo pacman -Sq --noconfirm --noprogressbar --needed --disable-download-timeout - \
+    <"$SETUP_DIR/requirements-pacman.txt"
 yay -Sq --noconfirm --noprogressbar --needed --disable-download-timeout - \
-    <"$SETUP_DIR/requirements.txt"
+    <"$SETUP_DIR/requirements-aur.txt"
 gum log -l info "[DONE] Installing packages"
