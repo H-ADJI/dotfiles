17bec4d06899975f3d547de4acf2614bb801755a
khalil hadji <h-adji_tech@proton.me>
2026-07-16 20:15:23 +0200

    fix: add prompts

--- arch/setup/main.sh
+++ arch/setup/main.sh
@@ -14,10 +14,24 @@ sudo pacman -S --noconfirm --noprogressbar --needed --disable-download-timeout "
 [ ! -d "dotfiles" ] && git clone --branch migration/multi_os_setup https://github.com/H-ADJI/dotfiles
 
 SETUP_DIR="dotfiles/arch/setup/lib"
+
+gum confirm "install AUR helper"
 bash "$SETUP_DIR/aur_helper.sh"
+
+gum confirm "install packages"
 bash "$SETUP_DIR/packages.sh"
+
+gum confirm "Setup projects"
 bash "$SETUP_DIR/projects.sh"
+
+gum confirm "Decrypt secrets"
 bash "$SETUP_DIR/secrets.sh"
+
+gum confirm "Link dotfiles"
 bash "$SETUP_DIR/dotfiles.sh"
+
+gum confirm "install extra packages"
 bash "$SETUP_DIR/extra_packages.sh"
+
+gum confirm "Set system state"
 bash "$SETUP_DIR/system_state.sh"
