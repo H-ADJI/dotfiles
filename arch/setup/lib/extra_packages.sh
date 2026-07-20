81a970e7c92dc3b585b18f6854084771c608dde0
khalil hadji <h-adji_tech@proton.me>
2026-07-20 15:13:03 +0200

    fix: requirements

--- arch/setup/lib/extra_packages.sh
+++ arch/setup/lib/extra_packages.sh
@@ -5,6 +5,6 @@ gum log -l info "[START] extra packages"
 mise i --locked
 nvim --headless -c 'Lazy install' -c 'MasonToolsInstallSync' -c 'qa'
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-sh ~/.tmux/plugins/tpm/bin/install_plugins
+bash ~/.tmux/plugins/tpm/bin/install_plugins
 
 gum log -l info "[DONE] extra packages"
