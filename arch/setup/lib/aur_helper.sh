6e6ee3d64274621926fa80ce243030335f6854dc
khalil hadji <h-adji_tech@proton.me>
2026-07-20 14:45:01 +0200

    update: yay install into tmp dir

--- arch/setup/lib/aur_helper.sh
+++ arch/setup/lib/aur_helper.sh
@@ -1,8 +1,10 @@
+set -euo pipefail
+
 gum log -l info "[START] AUR helper"
 
-[ ! -d "yay" ] && git clone --depth 1 https://aur.archlinux.org/yay.git
-cd "yay" || exit 1
-makepkg -si --noconfirm
-cd || exit 1
+YAY_DIR="/tmp/yay"
+git clone --depth 1 https://aur.archlinux.org/yay.git "$YAY_DIR"
+( cd "$YAY_DIR" && makepkg -si --noconfirm )
+rm -rf "$YAY_DIR"
 
 gum log -l info "[DONE] AUR helper"
