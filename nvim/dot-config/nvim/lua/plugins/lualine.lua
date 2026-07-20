c4d8166fba0cfbc3eb7b1fe700230d11b2d4abcb
khalil hadji <h-adji_tech@proton.me>
2026-07-20 16:11:14 +0200

    update: lualine and mise global conf

--- nvim/dot-config/nvim/lua/plugins/lualine.lua
+++ nvim/dot-config/nvim/lua/plugins/lualine.lua
@@ -6,7 +6,7 @@ return {
     sections = {
       lualine_a = { "mode" },
       lualine_b = { "branch", "diff", "diagnostics" },
-      lualine_c = { { "filename", path = 4 } },
+      lualine_c = { { "filename", path = 1 } },
       lualine_x = { "lsp_status" },
       lualine_y = { "progress", { "searchcount", maxcount = 9999, timeout = 500 } },
       lualine_z = { "location" },
