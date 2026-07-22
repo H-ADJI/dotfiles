ca73685edc91642438a1e354544513b1134b80f7
khalil hadji <h-adji_tech@proton.me>
2026-07-22 15:20:33 +0200

    add: colima path fix

--- macos/modules/home/colima.nix
+++ macos/modules/home/colima.nix
@@ -1,3 +1,10 @@
+{ lib, pkgs, ... }:
+
 {
   services.colima.enable = true;
+
+  # Colima needs gzip while unpacking its initial VM image. The upstream
+  # LaunchAgent PATH omits macOS's /usr/bin, where gzip is provided.
+  launchd.agents.colima-default.config.EnvironmentVariables.PATH = lib.mkForce
+    "${pkgs.colima}/bin:${pkgs.perl}/bin:${pkgs.docker}/bin:${pkgs.openssh}/bin:${pkgs.coreutils}/bin:${pkgs.curl}/bin:${pkgs.bashNonInteractive}/bin:${pkgs.kubectl}/bin:${pkgs.darwin.DarwinTools}/bin:/usr/bin:/bin";
 }
