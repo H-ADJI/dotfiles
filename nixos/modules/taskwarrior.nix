d166c85102d0ae236da62670fd22e7b3ff1a3418
khalil hadji <h-adji_tech@proton.me>
2026-08-03 23:47:48 +0200

    trim taskwarrior conf

--- nixos/modules/taskwarrior.nix
+++ nixos/modules/taskwarrior.nix
@@ -2,9 +2,6 @@
   programs.taskwarrior = {
     enable = true;
     colorTheme = "light-256";
-    extraConfig = ''
-      news.version=3.4.2
-    '';
     config = {
       confirmation = false;
     };
