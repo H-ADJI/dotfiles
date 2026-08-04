a70324770d1e9eef5b558a16daf8c1dd605266fb
khalil hadji <h-adji_tech@proton.me>
2026-08-04 19:54:23 +0200

    add sunsetr service

--- nixos/modules/sunsetr/default.nix
+++ nixos/modules/sunsetr/default.nix
@@ -1,8 +1,31 @@
-{ config, nixosModules, ... }:
+{
+  config,
+  pkgs,
+  nixosModules,
+  ...
+}:
 let
+  # TODO: home manager module
   sunsetr = "${nixosModules}/sunsetr/sunsetr.toml";
 in
 {
-
   xdg.configFile."sunsetr/sunsetr.toml".source = config.lib.file.mkOutOfStoreSymlink sunsetr;
+
+  systemd.user.services.sunsetr = {
+    Unit = {
+      Description = "Sunsetr - Automatic blue light filter for Hyprland, Niri, and everything Wayland";
+      Documentation = "https://github.com/psi4j/sunsetr";
+      PartOf = [ "graphical-session.target" ];
+      Requires = [ "graphical-session.target" ];
+      After = [ "graphical-session.target" ];
+      ConditionEnvironment = "WAYLAND_DISPLAY";
+    };
+    Service = {
+      Type = "simple";
+      ExecStart = "${pkgs.sunsetr}/bin/sunsetr";
+      Restart = "on-failure";
+      RestartSec = 30;
+    };
+    Install.WantedBy = [ "hyprland-session.target" ];
+  };
 }
