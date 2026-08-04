e48a67b896ec39b2423356f8d9591ef89b1b7a3f
khalil hadji <h-adji_tech@proton.me>
2026-08-04 23:18:56 +0200

    todos and missing modules

--- nixos/modules/hyprland/lib/autostart.lua
+++ nixos/modules/hyprland/lib/autostart.lua
@@ -1,11 +1,9 @@
 hl.on("hyprland.start", function()
     -- TODO: migrate to systemd services
-    -- hl.exec_cmd("dbus-update-activation-environment --systemd --all")
     hl.exec_cmd("swaync")
     hl.exec_cmd("hypridle")
     hl.exec_cmd("wl-paste --type text --watch cliphist store")
     hl.exec_cmd("wl-paste --type image --watch cliphist store")
-    hl.exec_cmd("nm-applet --indicator")
 end)
 
 hl.on("config.reloaded", function()
