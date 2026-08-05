46296cc83fcc84bed2a45b380e99a0f029619a26
khalil hadji <h-adji_tech@proton.me>
2026-08-05 09:08:37 +0200

    fix raffi conf

--- nixos/modules/raffi/default.nix
+++ nixos/modules/raffi/default.nix
@@ -5,7 +5,7 @@
 }:
 
 let
-  raffi_conf = "${nixosModules}/raffi/raffi.yml";
+  raffi_conf = "${nixosModules}/raffi/raffi.yaml";
 in
 {
   xdg.configFile."raffi/raffi.yml".source = config.lib.file.mkOutOfStoreSymlink raffi_conf;
