6cd6b051f010d1486b449c8b39d926bf84158454
khalil hadji <h-adji_tech@proton.me>
2026-08-06 20:36:02 +0200

    clear auto-start / reload

--- nixos/modules/hyprland/lib/autostart.lua
+++ nixos/modules/hyprland/lib/autostart.lua
@@ -1,8 +1,3 @@
-hl.on("hyprland.start", function()
-    -- TODO: migrate to systemd services
-end)
+hl.on("hyprland.start", function() end)
 
-hl.on("config.reloaded", function()
-    -- hl.exec_cmd("$HOME/.config/hypr/scripts/wellbeing --restart")
-    -- hl.exec_cmd("sleep 3; bluetoothctl connect 10:94:97:36:C7:15")
-end)
+hl.on("config.reloaded", function() end)
