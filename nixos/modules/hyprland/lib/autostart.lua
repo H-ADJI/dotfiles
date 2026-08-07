a4b7d85244f42414bb9025a1f70bbd48ba2ba8d9
khalil hadji <h-adji_tech@proton.me>
2026-08-07 22:50:04 +0200

    remove todo

--- nixos/modules/hyprland/lib/autostart.lua
+++ nixos/modules/hyprland/lib/autostart.lua
@@ -1,7 +1,3 @@
 hl.on("hyprland.start", function() end)
 
 hl.on("config.reloaded", function() end)
-
--- TODO:
--- on screenshare on  start call noctalia msg notification-dnd-set on / off https://wiki.hypr.land/Configuring/Advanced-and-Cool/Expanding-functionality/#events
-hl.on("screenshare.state", function() end)
