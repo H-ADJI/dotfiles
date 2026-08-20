e59b992b5a02ec595bbd888456bdb3cf72c5e4c2
khalil hadji <h-adji_tech@proton.me>
2026-08-20 20:03:10 +0200

    add flake alternative shell modules / profiles

--- flake.nix
+++ flake.nix
@@ -8,17 +8,21 @@
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
     in
     {
-      devShells.x86_64-linux.default = pkgs.mkShell {
-        packages = with pkgs; [
-          nixd
-          nixfmt
-          statix
-          lua-language-server
-          stylua
-          yaml-language-server
-          yamlfmt
-        ];
-        shellHook = "";
+      devShells.x86_64-linux = {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            nixd
+            nixfmt
+            statix
+            lua-language-server
+            stylua
+            yaml-language-server
+            yamlfmt
+          ];
+          shellHook = "";
+        };
+        # alternative shell profile : nix develop .#special
+        special = pkgs.mkShell { };
       };
     };
 }
