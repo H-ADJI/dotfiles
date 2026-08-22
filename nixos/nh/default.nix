b915ed0e4fb631f9127ad00aac6809238ef2fe11
khalil hadji <h-adji_tech@proton.me>
2026-08-22 16:45:13 +0200

    nh args tweak

--- nixos/nh/default.nix
+++ nixos/nh/default.nix
@@ -8,7 +8,7 @@
     clean = {
       enable = true;
       dates = "daily";
-      extraArgs = "--keep-since 7d --keep 5";
+      extraArgs = "--keep 5";
     };
     flake = nixosModules;
     osFlake = nixosModules;
--- nixos/zsh/default.nix
+++ nixos/zsh/default.nix
@@ -76,7 +76,7 @@
       nvim_shada_clear = "rm ~/.local/state/nvim/shada/main.shada";
       hd = "hunk diff";
       nix_switch = "sudo nixos-rebuild switch --flake ~/PDE/nixos#nixos";
-      nh_clean = "nh clean all --keep-since 7d --keep 5";
+      nh_clean = "nh clean all --keep 5";
       nh_switch = "nh os switch --accept-flake-config";
     };
 
