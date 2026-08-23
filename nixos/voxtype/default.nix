cac7f998446a4ff37172274828491b0cdb42731f
khalil hadji <h-adji_tech@proton.me>
2026-08-23 17:16:45 +0200

    voxtype conf

--- nixos/voxtype/default.nix
+++ nixos/voxtype/default.nix
@@ -1,4 +1,31 @@
+{ ... }:
 {
-  # TODO: configure
-  services.voxtype.enable = true;
+  services.voxtype = {
+    enable = true;
+    loadModels = [ "tiny.en" ];
+    wayland.display = "wayland-1";
+    settings = {
+      hotkey.enabled = false;
+      engine = "whisper";
+      audio = {
+        device = "default";
+        sample_rate = 16000;
+        max_duration_secs = 120;
+      };
+      whisper = {
+        model = "tiny.en";
+        language = "en";
+        translate = false;
+      };
+      output = {
+        mode = "type";
+        fallback_to_clipboard = true;
+        notification = {
+          on_recording_start = false;
+          on_recording_stop = false;
+          on_transcription = false;
+        };
+      };
+    };
+  };
 }
