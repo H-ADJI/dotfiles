06f5cd2fc768fa840d63e18edf0ccbce898bec62
khalil hadji <h-adji_tech@proton.me>
2026-08-25 13:42:46 +0200

    remove comment

--- nixos/tmux/default.nix
+++ nixos/tmux/default.nix
@@ -10,7 +10,6 @@
 
   xdg.configFile = {
     "tmux/tmux.conf".source = ./tmux.conf;
-    # "tmux/status-bar.conf".source = ./status-bar.conf;
 
     "tmux/scripts/md_preview" = {
       source = ./scripts/md_preview;
