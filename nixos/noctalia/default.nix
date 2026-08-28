c4af8dce63754652e451bb49b791915de77a01d1
khalil hadji <h-adji_tech@proton.me>
2026-08-28 11:52:19 +0200

    reduce volume while transcribing

--- nixos/noctalia/default.nix
+++ nixos/noctalia/default.nix
@@ -10,8 +10,4 @@
   xdg.configFile."noctalia/config.toml".source = ./noctalia.toml;
   xdg.configFile."noctalia/conf".source = ./conf;
 
-  # Local noctalia plugin (highest-precedence data-dir drop-in).
-  xdg.dataFile."noctalia/plugins/khalil/default-mic/plugin.toml".source = ./plugins/default-mic/plugin.toml;
-  xdg.dataFile."noctalia/plugins/khalil/default-mic/widget.luau".source = ./plugins/default-mic/widget.luau;
-
 }
