d2dfa4e632eeaa0e0eb9bdbb209f001222862f8a
khalil hadji <h-adji_tech@proton.me>
2026-09-02 11:25:27 +0200

    move packages list

--- macos/home.nix
+++ macos/home.nix
@@ -38,6 +38,37 @@
   home.stateVersion = "26.05";
 
   home.packages = with pkgs; [
+    aerospace
+    alacritty
     brave
+    cargo
+    clang
+    coreutils
+    curl
+    docker
+    docker-compose
+    fd
+    fzf
+    gh
+    glow
+    gnugrep
+    gnutar
+    go
+    google-chrome
+    hunk
+    hyperfine
+    mise
+    nerd-fonts.jetbrains-mono
+    nodejs
+    podman
+    podman-compose
+    python3
+    ripgrep
+    rustc
+    stow
+    transcrypt
+    tree
+    usage
+    uv
   ];
 }
