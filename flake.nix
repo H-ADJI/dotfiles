0324b3b0dce96a52980d7ef4e7e2aa946e1040c7
khalil hadji <h-adji_tech@proton.me>
2026-09-08 10:08:02 +0200

    nix darwin home manager

--- flake.nix
+++ flake.nix
@@ -3,32 +3,21 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
     home-manager = {
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     nix-darwin = {
       url = "github:nix-darwin/nix-darwin/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     nix-homebrew.url = "github:zhaofengli/nix-homebrew";
-
     noctalia = {
       url = "github:noctalia-dev/noctalia/cachix";
     };
-
     musnix = {
       url = "github:musnix/musnix";
     };
-
-    nox = {
-      url = "github:madsbv/nix-options-search";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     hyprland-preview-share-picker = {
       url = "git+https://github.com/WhySoBad/hyprland-preview-share-picker.git?submodules=1";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -51,30 +40,19 @@
         specialArgs = { inherit inputs; };
         modules = [
           ./nixos/configuration.nix
-          ./nixos/home.nix
-          home-manager.nixosModules.home-manager
           musnix.nixosModules.musnix
+          home-manager.nixosModules.home-manager
+          ./nixos/home.nix
         ];
       };
 
       darwinConfigurations.macbook = nix-darwin.lib.darwinSystem {
         specialArgs = { inherit inputs; };
         modules = [
-          ./macos/configuration.nix
           nix-homebrew.darwinModules.nix-homebrew
+          ./macos/configuration.nix
           home-manager.darwinModules.home-manager
-          {
-            home-manager = {
-              extraSpecialArgs = {
-                inherit inputs;
-              };
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              users = {
-                khalil = import ./macos/home.nix;
-              };
-            };
-          }
+          ./macos/home.nix
         ];
       };
     };
--- macos/home.nix
+++ macos/home.nix
@@ -2,48 +2,55 @@
   pkgs,
   ...
 }:
-{
-  imports = [
-    ../common/alacritty
-    ../common/direnv
-    ../common/fastfetch
-    ../common/ghostty
-    ../common/git
-    ../common/glow
-    ../common/hunk
-    ../common/jnv
-    ../common/jqp
-    ../common/mise
-    ../common/nh
-    ../common/nvim
-    ../common/opencode
-    ../common/pi-agent
-    ../common/ssh
-    ../common/starship
-    ../common/tabiew
-    ../common/taskwarrior
-    ../common/television
-    ../common/tmux
-    ../common/yazi
-    ../common/zsh
-    ./aerospace
-    ./colima
-    ./ghostty
-    ./sketchybar
-    ./zsh
-  ];
 
-  home = {
-    username = "khalil";
-    homeDirectory = "/Users/khalil";
-    stateVersion = "26.05";
-    file.".hushlogin".text = "";
-    packages = with pkgs; [
-      docker
-      docker-compose
-      google-chrome
-      brave
-      nerd-fonts.jetbrains-mono
-    ];
+{
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    users.khalil = {
+      imports = [
+        ../common/alacritty
+        ../common/direnv
+        ../common/fastfetch
+        ../common/ghostty
+        ../common/git
+        ../common/glow
+        ../common/hunk
+        ../common/jnv
+        ../common/jqp
+        ../common/mise
+        ../common/nh
+        ../common/nvim
+        ../common/opencode
+        ../common/pi-agent
+        ../common/ssh
+        ../common/starship
+        ../common/tabiew
+        ../common/taskwarrior
+        ../common/television
+        ../common/tmux
+        ../common/yazi
+        ../common/zsh
+        ./aerospace
+        ./colima
+        ./ghostty
+        ./sketchybar
+        ./zsh
+      ];
+
+      home = {
+        username = "khalil";
+        homeDirectory = "/Users/khalil";
+        stateVersion = "26.05";
+        file.".hushlogin".text = "";
+        packages = with pkgs; [
+          docker
+          docker-compose
+          google-chrome
+          brave
+          nerd-fonts.jetbrains-mono
+        ];
+      };
+    };
   };
 }
